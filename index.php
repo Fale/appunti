5aeb1abc0983c26623a937847692a8d098992e3f
Fabio Alessandro Locati <flocati@grimp.eu>
2011-03-13 22:31:20 +0100

    better urls

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ file::head("Fale's Univesity Home");
 ?>
 
 <ul>
-<li><a href="Filosofia Teoretica">Filosofia Teoretica</a>
-<li><a href="Storia della Filosofia Moderna">Storia della Filosofia Moderna</a>
-<li><a href="Storia della Filosofia Morale">Storia della Filosofia Morale</a>
+<li><a href="Filosofia_Teoretica">Filosofia Teoretica</a>
+<li><a href="Storia_della_Filosofia_Moderna">Storia della Filosofia Moderna</a>
+<li><a href="Storia_della_Filosofia_Morale">Storia della Filosofia Morale</a>
 </ul>
 
 <?php file::footer(); ?>

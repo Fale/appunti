4ebb616bf45905f4096eb4782ce51fe99dc4c5bc
Fabio Alessandro Locati <flocati@grimp.eu>
2011-03-14 22:48:16 +0100

    Add indexes

---
+++ Filosofia_Teoretica/index.php
@@ -0,0 +1,11 @@
+<?php
+require "class/include.php";
+
+file::head("Fale's Univesity Home");
+?>
+
+<ul>
+<li><a href="appunti.php">Appunti</a>
+</ul>
+
+<?php file::footer(); ?>
---
+++ Storia_della_Filosofia_Morale/index.php
@@ -0,0 +1,12 @@
+<?php
+require "class/include.php";
+
+file::head("Fale's Univesity Home");
+?>
+
+<ul>
+<li><a href="appunti.php">Appunti</a>
+<li><a href="Totem_e_tabu.php">Totem e Tabù</a>
+</ul>
+
+<?php file::footer(); ?>

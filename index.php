05bad0b2235151ff488952d678993db02237e8cb
Fabio Alessandro Locati <flocati@grimp.eu>
2011-04-01 23:34:09 +0200

    Add Gnoseologia to list

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ file::head("Fale's Univesity Home");
 
 <ul>
 <li><a href="Filosofia_Teoretica">Filosofia Teoretica</a>
+<li><a href="Gnoseologia">Gnoseologia</a>
 <li><a href="Storia_della_Filosofia_Moderna">Storia della Filosofia Moderna</a>
 <li><a href="Storia_della_Filosofia_Morale">Storia della Filosofia Morale</a>
 </ul>

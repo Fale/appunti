689d39d317eb32cb401a8819a23ba4b753ea4f19
Fabio Alessandro Locati <flocati@grimp.eu>
2011-04-01 23:35:19 +0200

    Create index for Gnoseologia

---
+++ Gnoseologia/index.php
@@ -0,0 +1,12 @@
+<?php
+require "../class/include.php";
+
+file::head("Fale's Univesity Home");
+?>
+
+<ul>
+<li><a href="appunti1.html">Appunti - Parte 1</a>
+<li><a href="appunti2.html">Appunti - Parte 2</a>
+</ul>
+
+<?php file::footer(); ?>

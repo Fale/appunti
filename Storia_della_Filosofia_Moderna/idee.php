1735e84d3797e210b8df33837f2e6e81731a9ba9
Fabio Alessandro Locati <flocati@grimp.eu>
2011-06-07 11:55:33 +0200

    Add new file to Storia della Filosofia Moderna with the ideas divided by author

---
+++ Storia_della_Filosofia_Moderna/idee.php
@@ -0,0 +1,11 @@
+<?php
+require "../class/include.php";
+
+file::head("Idee");
+
+toc::show_toc("Storia_della_Filosofia_Moderna/idee.php");
+?>
+
+<?php t("1", "Aristotele"); ?>
+
+<?php file::footer(); ?>
--- Storia_della_Filosofia_Moderna/index.php
+++ Storia_della_Filosofia_Moderna/index.php
@@ -5,7 +5,8 @@ file::head("Fale's Univesity Home");
 ?>
 
 <ul>
-<li><a href="appunti.php">Appunti</a>
+<li><a href="appunti.php">Appunti</a></li>
+<li><a href="idee.php">Idee</a></li>
 </ul>
 
 <?php file::footer(); ?>

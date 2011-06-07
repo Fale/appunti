ff4b02fd24d2652c7998eac288532a936684d7bf
Fabio Alessandro Locati <flocati@grimp.eu>
2011-06-07 12:05:19 +0200

    Add Aristotele stuff

--- Storia_della_Filosofia_Moderna/idee.php
+++ Storia_della_Filosofia_Moderna/idee.php
@@ -7,5 +7,19 @@ toc::show_toc("Storia_della_Filosofia_Moderna/idee.php");
 ?>
 
 <?php t("1", "Aristotele"); ?>
-
+<p><b>Modello:</b> geocentrico</p>
+<p><b>Descrizione:</b><br />
+La Terra è formata da quattro elementi:
+<ul>
+    <li>la terra</li>
+    <li>l'aria</li>
+    <li>il fuoco</li>
+    <li>l'acqua</li>
+</ul>
+Ogni elemento ha la tendenza a rimanere o a tornare nel proprio luogo naturale, che per la terra e l'acqua è il basso, mentre per l'aria e il fuoco è l'alto. La Terra come pianeta, quindi, non può che stare al centro dell'universo, poiché è formata dai due elementi tendenti al basso, e il "basso assoluto" è proprio il centro dell'universo.
+Riguardo a ciò che si trova oltre la Terra, Aristotele lo riteneva composto di un quinta essenza: l'etere. L'etere, che non esiste sulla Terra, sarebbe privo di massa, invisibile e, soprattutto, eterno ed inalterabile: queste due ultime caratteristiche sanciscono un confine tra i luoghi sub-lunari del mutamento (la Terra) e i luoghi immutabili (il cosmo).</p>
+<p><b>Ordine planetario</b>:Oltre la Terra per lui vi erano, in ordine, la Luna, Mercurio, Venere, il Sole, Marte, Giove, Saturno, il cielo delle stelle fisse e, infine, il primo mobile, che metteva tutte le altre sfere in movimento.</p>
+<p><b>Movimento:</b> i corpi celesti si muovono su sfere concentriche (in numero di cinquantacinque, ventidue in più delle 33 di Callippo). Il movimento era dato dal primo mobile, ultima sfera. Esso risulta mosso direttamente dalla causa prima, identificabile con la divinità suprema (mentre le altre divinità risiedevano all'interno del cosmo).</p>
+<p><b>Extra universo:</b> unicità e finitezza dell'universo: l'unicità perché se esistesse un altro universo sarebbe composto sostanzialmente dei medesimi elementi del nostro, i quali tenderebbero, per i luoghi naturali, ad avvicinarsi al nostro fino a ricongiungersi completamente con esso, ciò che prova l'unicità del nostro universo; la finitezza perché in uno spazio infinito non potrebbe esistere alcun centro, ciò che contravverrebbe alla teoria dei luoghi naturali.
+</p>
 <?php file::footer(); ?>

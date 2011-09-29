f2b1159bf164683d8c027b37e731284af6eeb4ed
Fabio Alessandro Locati <flocati@grimp.eu>
2011-09-29 10:49:37 +0200

    Add lecture 1 of Storia della Filosofia

--- Storia_della_Filosofia/appunti.php
+++ Storia_della_Filosofia/appunti.php
@@ -7,5 +7,30 @@ toc::show_toc("Storia_della_Filosofia/appunti.php");
 ?>
 
 <?php t("1", "27/09/2011"); ?>
+<p>Testi:</p>
+<ul>
+  <li>Modelli di Filosofia Politica (Petulciani)</li>
+  <p>Testo di introduzione alla Filosofia Politica</p>
+  <li>Introduzione al pensiero politico antico (Gastaldi)</li>
+  <p>Speicalemnte Aristotele e Platone</p>
+  <li>Aristotele</li>
+  <ul>
+    <li>Antologia del pensiero politico (testo: La Politica)</li>
+    <li>Profilo di Aristotele (consigliato per filo)</li>
+  </ul>
+  <li>Hobbs</li>
+  <ul>
+    <li>Il cittadino (bobbio)</li>
+  </ul>
+</ul>
+
+<?php t2("1.1", "Introduzione"); ?>
+<p>Aristotele è il primo a organizzare le scienze. Stabilisce la relativa autonomia della Filosofia Politica rispetto ad altri ambiti della Filosofia e di altri ambiti del sapere. anche per Aristotele, la politca non è separata dalle altre discipline, è divisa, ma non è completamente divisa ad esempio con l'etica. Il buon cittadino non coincide con l'uomo onesto, ma le due cose non sono completamente separate. Nel VI secolo AC nasce lo spazio politico della discussione pubblico, nel senso di democrazia. Quindi qui nasce la retorica. Aristotele tratta in modo diviso la retorica, la politica e l'etica.</p>
+<p>Aristotele stabilisce i confini e l'oggetto della Filosofia Politica. Weber nel 1919 a Monaco conferenze "La scienza come vocazione" e "La politica come vocazione". L'etica dei principi (o delle intenzioni) e l'etica della responsabilità (scendere a patti, occupandosi anche delle conseguenze). "Lo stato è come le associazioni politiche che storicamente lo precedono, un rapporto di dominio di uomini su uomini, è il potere che alcuni uomini esercitano su altri uomini, ed è fondato sul mezzo della forza legittima (cioè considerata legittima)". Per Marx, è il potere economico quello primario, altri sostengono quello familiare o micropoteri. I cittadini non possono usare la forza perchè essa diventa monopolio del potere statale. La Filosofia Politica si pone il problema del ottimo e giusto di esercitare il potere da Platone.</p>
+<p>Platone ha un aproccio molto normativo per quanto riguarda la politica. Secondo Platone, anche le donne possono governare. Nel IX libro, Platone afferma che esiste il paradigma in cielo a cui noi dobbiamo sempre tendere. Per questo Platone è stato accusato di utopia. La componente normativa in Hobbs, si presenta nelle utopie e nella domanda circa l'origine e la legittimità del potere.</p>
+<p>Per Platone e Aristotele, lo stato è il prolungamento della natura umana. L'uomo è naturalemnte e spontaneamente un animale politico, cioè vive in comunità. Nell'età moderna viene fuori lo stato di natura, cioè una condizione naturale dell'uomo che è una situazione pre-politica. A questo punto si passa ad una legittimazione del potere, se conforme alla legge di natura, derivante dal basso. Catrattualismo e naturalismo permettono una fortissima legittimazione del potere. Lock afferma che il potere è legittimo è quello verso i quali viene rispettato e garantisce ai cittadini il godimento dei loro diritti naturali.</p>
+
+<?php t("2", "29/09/2011"); ?>
+<p></p>
 
 <?php file::footer(); ?>

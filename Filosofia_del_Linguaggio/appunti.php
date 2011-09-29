1480c2bb080dd3d721908d5304be4d99d9ebe23a
Fabio Alessandro Locati <flocati@grimp.eu>
2011-09-29 10:12:03 +0200

    Add file appunti.php for Filosofia del Linguaggio

---
+++ Filosofia_del_Linguaggio/appunti.php
@@ -0,0 +1,11 @@
+<?php
+require "../class/include.php";
+
+file::head("Filosofia del Linguaggio");
+
+toc::show_toc("Filosofia_del_Linguaggio/appunti.php");
+?>
+
+<?php t("2", "29/09/2011"); ?>
+
+<?php file::footer(); ?>

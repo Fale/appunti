b48949a63505008630af0e88c12870aad3a8b0cc
Fabio Alessandro Locati <flocati@grimp.eu>
2011-09-29 10:10:36 +0200

    Add file appunti.php for Storia della Filosofia

---
+++ Storia_della_Filosofia/appunti.php
@@ -0,0 +1,11 @@
+<?php
+require "../class/include.php";
+
+file::head("Storia della Filosofia");
+
+toc::show_toc("Storia_della_Filosofia/appunti.php");
+?>
+
+<?php t("1", "27/09/2011"); ?>
+
+<?php file::footer(); ?>

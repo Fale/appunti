530a657e71c4f6e00e5e09206717d7ab719000dd
Fabio Alessandro Locati <flocati@grimp.eu>
2011-10-04 18:14:13 +0200

    Remove a temp file

--- Epistemologia_delle_Scienze_Umane/appunti.php
+++ Epistemologia_delle_Scienze_Umane/appunti.php
@@ -8,4 +8,10 @@ toc::show_toc("Epistemologia_delle_Scienze_Umane/appunti.php");
 
 <?php t("2", "04/10/2011"); ?>
 <p>Nella Filosofia della Psicologia ha il doppio problema di dover analizzare opere che analizzavano altro. Grandi problemi a livello di psicologia, sorgono e persistono a volte per usi di categorie filosofiche inappropriate. Ci interessa la cognizione in tutta la sua ampietà. La Filosofia della Psicologia tende ad essere nell'ambito analitico, quindi si considera come "Analisi Concettuale", quindi a priori. La Filosofia della Psicologia tratta dei concetti psicologi, poco importa la ricerca psicologica in se, ma l'importante è dare una cornice teorica entro quale la psicologia ha senso. </p>
+<p>I due livelli:</p>
+<ul>
+  <li>Emozioni fondamentali
+  <li>All'interno della ricerca filosofica e dalla prova empirica assieme
+</ul>
+<p>Poichè non è un'analisi a priori, l'idea è andare a vedere il significato che queste assumono nei domini usati.</p>
 <?php file::footer(); ?>

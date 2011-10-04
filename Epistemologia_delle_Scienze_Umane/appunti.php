549c34f66ffd388141aae6027fa209ae2061c4a9
Fabio Alessandro Locati <flocati@grimp.eu>
2011-10-04 18:13:05 +0200

    Add lecture 2 of Epistemologia delle Scienze Umane

---
+++ Epistemologia_delle_Scienze_Umane/appunti.php
@@ -0,0 +1,11 @@
+<?php
+require "../class/include.php";
+
+file::head("Epistemologia");
+
+toc::show_toc("Epistemologia_delle_Scienze_Umane/appunti.php");
+?>
+
+<?php t("2", "04/10/2011"); ?>
+<p>Nella Filosofia della Psicologia ha il doppio problema di dover analizzare opere che analizzavano altro. Grandi problemi a livello di psicologia, sorgono e persistono a volte per usi di categorie filosofiche inappropriate. Ci interessa la cognizione in tutta la sua ampietà. La Filosofia della Psicologia tende ad essere nell'ambito analitico, quindi si considera come "Analisi Concettuale", quindi a priori. La Filosofia della Psicologia tratta dei concetti psicologi, poco importa la ricerca psicologica in se, ma l'importante è dare una cornice teorica entro quale la psicologia ha senso. </p>
+<?php file::footer(); ?>
